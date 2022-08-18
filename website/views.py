09c7ad2102875497102a2ba8f78988fa8a8f90b2
theharez <rahimi.alireza2109@gmail.com>
2022-08-18 19:14:05 +0430

    render index.html
    rendring a html file instead of HttpResponce for Home page

--- website/views.py
+++ website/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+""" def home(request):
+    return HttpResponse('<h1>Home page</h1>') """
+    
 def home(request):
-    return HttpResponse('<h1>Home page</h1>')
+    return render(request, 'code\index.html')    
 
 def about(request):
     return HttpResponse('<h1>About us</h1>')
