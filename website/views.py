1a8c16a1401a719e2512120458ffbceb4e34e0d6
theharez <rahimi.alireza2109@gmail.com>
2022-08-25 12:26:56 +0430

    update new path
    updating new html path

--- website/views.py
+++ website/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
     return HttpResponse('<h1>Home page</h1>') """
     
 def home(request):
-    return render(request, 'index.html')    
+    return render(request, 'main_page/index.html')    
 
 def about(request):
     return HttpResponse('<h1>About us</h1>')
