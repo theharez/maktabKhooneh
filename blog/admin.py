9ea7a961d33dd37b7a480555436b7da5b69d854b
theharez <rahimi.alireza2109@gmail.com>
2022-09-09 22:41:10 +0430

    Update admin.py
    update admin panel
    new admin.py class : Post_Admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from blog.models import Post
 
 # Register your models here.
-admin.site.register(Post)
+class Post_Admin(admin.ModelAdmin):
+    
+    date_hierarchy = 'created_data' # posts based on created_data or other DateTimeField 
+    empty_value_display = '-'   # unpublished posts
+    list_display = ('title', 'status', 'created_data', 'views', 'published_data')   # columns to show 
+    list_filter = ('status', )  # acsending or deseding sorting
+    ordering = ('created_data',)    # ordering by
+    search_fields = ['title', 'content']    # looking for an expression in the title or the content
+    
+admin.site.register(Post, Post_Admin)
 
