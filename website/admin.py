bdf17c334f51d40bcb39dd927ebd72ed63f12aee
theharez <rahimi.alireza2109@gmail.com>
2022-09-13 02:36:46 +0430

    update contact
    update website contact module

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from website.models import Contact
+
+class Contact_Admin(admin.ModelAdmin):
+    
+    date_hierarchy = 'created_date'
+    list_display = ('name', 'subject', 'created_date')
+    list_filter = ('email',)
+    search_fields = ['name', 'email']
 
 # Register your models here.
+admin.site.register(Contact, Contact_Admin)
--- website/models.py
+++ website/models.py
@@ -9,5 +9,7 @@ class Contact(models.Model):
     message = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-     
+    
+    def __str__(self):
+        return (f'{self.name} /// {self.email}')
     
