2b4e5fda4cb7293683e694c9c199916261836adb
theharez <rahimi.alireza2109@gmail.com>
2022-09-18 13:35:32 +0430

    update blog views
    connecting blog home page to blog single page

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,15 @@
-from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.utils import timezone
 
 def blog_home_view(request):
-    posts = Post.objects.filter(status=True)
-    context = {'posts': posts}
+    posts = Post.objects.filter(published_data__lte=timezone.now(), status=True)
+    context = {'posts': posts} 
     return render(request, 'blog/blog-home.html', context)
 
-def blog_single_view(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single_view(request, pid):
+    single = get_object_or_404(Post, id=pid, status=True)
+    single.views +=1 
+    single.save()
+    context = {'single': single}
+    return render(request, 'blog/blog-single.html', context)
