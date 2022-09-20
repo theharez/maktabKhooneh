35ba626440721e3d739cd78dfba76db1cbbe259c
theharez <rahimi.alireza2109@gmail.com>
2022-09-21 01:13:02 +0430

    update model
    update blog default image

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     views = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True) # or (on_delete=models.SET_NULL)
     status = models.BooleanField(default=False)
-    image = models.ImageField(upload_to='blog/', default='blog\\default.jpg')
+    image = models.ImageField(upload_to='blog/', default='blog/2switch.png')
     created_data = models.DateTimeField(auto_now_add=True)
     updated_data = models.DateTimeField(auto_now=True)
     published_data = models.DateTimeField(null=True)
