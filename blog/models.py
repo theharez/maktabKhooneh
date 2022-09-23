44a2c0f7aff0926df8f31ab537a461cdc748ebe2
theharez <rahimi.alireza2109@gmail.com>
2022-09-23 22:20:40 +0330

    Update models.py
    adding blog category

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,20 @@
 
+from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
 class Post(models.Model):
 
     title = models.CharField(max_length=255)
     content = models.TextField()
     views = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True) # or (on_delete=models.SET_NULL)
+    category = models.ManyToManyField(Category)
     status = models.BooleanField(default=False)
     image = models.ImageField(upload_to='blog/', default='blog/2switch.png')
     created_data = models.DateTimeField(auto_now_add=True)
