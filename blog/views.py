8acdf308dc8d3a6a7ff0c002ed822738607dff6e
theharez <rahimi.alireza2109@gmail.com>
2022-10-02 15:20:25 +0330

    Update views.py
    add blog search function
    add post author function

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from blog.models import Post
 from django.utils import timezone
 
-def blog_home_view(request, cat_name=None):    
+def blog_home_view(request, **kwargs):    
     posts = Post.objects.filter(published_data__lte=timezone.now(), status=True)
-    if cat_name:
-        posts = posts.filter(category__name = cat_name)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_name') != None:
+        posts = posts.filter(author__username=kwargs['author_name'])
     context = {'posts': posts} 
     return render(request, 'blog/blog-home.html', context)
 
@@ -26,6 +28,14 @@ def blog_single_view(request, pid):
     except:
         context.update({'single': single})
     return render(request, 'blog/blog-single.html', context)
+
+def blog_search(request):
+    posts = Post.objects.filter(published_data__lte=timezone.now(), status=True)
+    if request.method == 'GET':
+        if search := request.GET.get('search'):
+            posts = posts.filter(content__contains=search)
+    context = {'posts': posts} 
+    return render(request, 'blog/blog-home.html', context)
 
 
 def test(request):
