5687c1fa153dcf90663c44f6367537768326f00a
theharez <rahimi.alireza2109@gmail.com>
2022-10-02 13:00:31 +0330

    Update blog_custome_tags.py
    add latest post tag
    add category tag

--- blog/templatetags/blog_custome_tags.py
+++ blog/templatetags/blog_custome_tags.py
@@ -1,26 +1,43 @@
+from unicodedata import category
 from django import template
 from blog.models import Post
+from blog.models import Category
 register = template.Library()
 
-@register.simple_tag
+@register.simple_tag()
 def say_hi():
     return 'Hi budy'
+
 
 @register.simple_tag(name='plus_self')
 def sum(NO):
     return NO + NO
 
+
 @register.simple_tag(name='post_count')
 def func():
     posts = Post.objects.filter(status=1).count()
+    
     return posts
+
 
 @register.simple_tag(name='posts')
 def func():
     posts = Post.objects.filter(status=1)
     return posts
 
-@register.inclusion_tag('blog/blog-latest-posts.html')
+
+@register.inclusion_tag('blog/blog-latest-posts.html', name='latest')
 def latest_posts():
-    posts = Post.objects.filter(status=1).order_by('-views')[:2]
+    posts = Post.objects.filter(status=1).order_by('-published_data')[:2]
     return {'posts': posts}
+
+
+@register.inclusion_tag('blog/blog-post-category.html', name='category')
+def post_categories():    
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = dict()
+    for item in categories:
+        cat_dict[item] = posts.filter(category=item).count()
+    return {'categories': cat_dict}
