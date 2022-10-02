a09c03ae55894431c91127811ab90c5976601592
theharez <rahimi.alireza2109@gmail.com>
2022-10-02 15:19:50 +0330

    Update urls.py
    add new url to filtering based on author
    add client search url

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', blog_home_view, name='blog_home_view'),
     path('<int:pid>', blog_single_view, name='blog_single_view'),
     path('category/<str:cat_name>', blog_home_view, name='category'),
+    path('author/<str:author_name>', blog_home_view, name='author'),
+    path('search/', blog_search, name='search'),
     path('test', test, name='test')   
 ]
