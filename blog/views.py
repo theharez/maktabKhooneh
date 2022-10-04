b58795f607dc2df6540505e0ca21a92ae40ea315
theharez <rahimi.alireza2109@gmail.com>
2022-10-04 23:06:07 +0330

    Update views.py
    add post paginator

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from blog.models import Post
 from django.utils import timezone
+from django.core.paginator import Paginator, InvalidPage
 
 def blog_home_view(request, **kwargs):    
     posts = Post.objects.filter(published_data__lte=timezone.now(), status=True)
+        
     if kwargs.get('cat_name') != None:
         posts = posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_name') != None:
         posts = posts.filter(author__username=kwargs['author_name'])
+        
+    posts = Paginator(posts, 3)
+    page_number = request.GET.get('page')
+    try:
+        posts = posts.page(page_number)
+    except (InvalidPage):
+        posts = posts.page(1)
+        
     context = {'posts': posts} 
     return render(request, 'blog/blog-home.html', context)
 
@@ -17,23 +27,29 @@ def blog_single_view(request, pid):
     single.views +=1 
     single.save()
     context = {'single': single}
+        
     try:
         next = get_list_or_404(Post, id__gt=single.id, published_data__lte=timezone.now(), status=True)
         context.update({'single': single, 'next':next[0]})
     except:
         context = {'single': single}
+        
     try:
         prev = get_list_or_404(Post, id__lt=single.id, published_data__lte=timezone.now(), status=True)
         context.update({'single': single, 'prev':prev[-1]})
     except:
         context.update({'single': single})
+        
     return render(request, 'blog/blog-single.html', context)
+
 
 def blog_search(request):
     posts = Post.objects.filter(published_data__lte=timezone.now(), status=True)
+        
     if request.method == 'GET':
         if search := request.GET.get('search'):
             posts = posts.filter(content__contains=search)
+            
     context = {'posts': posts} 
     return render(request, 'blog/blog-home.html', context)
 
