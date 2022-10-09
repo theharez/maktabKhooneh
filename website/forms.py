ef66278400652e6b4570cbd5213ef979a056f21f
theharez <rahimi.alireza2109@gmail.com>
2022-10-10 00:08:46 +0330

    Update forms.py

--- website/forms.py
+++ website/forms.py
@@ -4,7 +4,9 @@ from website.models import Contact, news_letter
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
+
         fields = '__all__'
+
 
 class news_letter_form(forms.ModelForm):
     class Meta:
