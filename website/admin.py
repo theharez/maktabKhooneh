ecce735b80a8ece19606c28f3b67fd17db391902
theharez <rahimi.alireza2109@gmail.com>
2022-10-09 13:15:10 +0330

    Update admin.py
    update admin contact form panel
    add news letter section

--- website/admin.py
+++ website/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from website.models import Contact
+from website.models import Contact, news_letter
+from django.db import models
+from django.forms import Textarea
 
 class Contact_Admin(admin.ModelAdmin):
     
     date_hierarchy = 'created_date'
-    list_display = ('name', 'subject', 'created_date')
+    list_display = ('name', 'subject', 'email', 'phone', 'created_date')
     list_filter = ('email',)
     search_fields = ['name', 'email']
+    formfield_overrides = { models.SmallIntegerField: {'widget': Textarea(attrs={'rows':1, 'cols':20})}}
 
 # Register your models here.
 admin.site.register(Contact, Contact_Admin)
+admin.site.register(news_letter)
