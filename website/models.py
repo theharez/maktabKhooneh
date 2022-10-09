711e25c459a0dcca277d0819d354da0707ecfd62
theharez <rahimi.alireza2109@gmail.com>
2022-10-09 13:16:24 +0330

    Update models.py
    add contact phone number
    add news letter

--- website/models.py
+++ website/models.py
@@ -1,10 +1,9 @@
-from email import message
-from unicodedata import name
 from django.db import models
 
 class Contact(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
+    phone = models.SmallIntegerField(null=True)
     subject = models.CharField(max_length=255)
     message = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
@@ -12,4 +11,9 @@ class Contact(models.Model):
     
     def __str__(self):
         return (f'{self.name} /// {self.email}')
+
+class news_letter(models.Model):
+    email = models.EmailField()
     
+    def __str__(self):
+        return self.email
