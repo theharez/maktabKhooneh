5420df53d1039a07270e8771a8321b2759529b9d
theharez <rahimi.alireza2109@gmail.com>
2022-10-09 13:17:03 +0330

    Update urls.py
    add news letter url

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,6 @@ app_name = 'website'
 urlpatterns = [
     path('', home_page, name='home_view'),
     path('contact', contact_page, name='contact_view'),
-    path('about', about_page, name='about_view')
+    path('about', about_page, name='about_view'),
+    path('news-letter', news_letter_page, name='news_letter')
 ]
