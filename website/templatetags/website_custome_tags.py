f99a8d0b8e13ed4166c43695a6c6b97e7b4800ea
theharez <rahimi.alireza2109@gmail.com>
2022-10-09 13:16:40 +0330

    Update website_custome_tags.py
    update main page latest post

--- website/templatetags/website_custome_tags.py
+++ website/templatetags/website_custome_tags.py
@@ -1,10 +1,9 @@
 from django import template
 from blog.models import Post
-from blog.models import Category
 from django.utils import timezone
 register = template.Library()
 
 @register.inclusion_tag('main_page/index-latest-posts.html')
 def website_latest_posts():
-    posts = Post.objects.filter(published_data__lte=timezone.now(), status=True)
+    posts = Post.objects.filter(published_data__lte=timezone.now(), status=True).order_by('-published_data')[:6]
     return {'posts': posts}
