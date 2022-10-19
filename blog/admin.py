1a68558dcdcebd81f731dc738d07e82c661dec48
theharez <rahimi.alireza2109@gmail.com>
2022-10-19 14:06:29 +0330

    Update admin.py
    add text editor
    change Post_Admin class parent

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from blog.models import Post, Category
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-class Post_Admin(admin.ModelAdmin):
+class Post_Admin(SummernoteModelAdmin):
     
     date_hierarchy = 'created_data' # posts based on created_data or other DateTimeField 
     empty_value_display = '-'   # unpublished posts
@@ -10,6 +11,7 @@ class Post_Admin(admin.ModelAdmin):
     list_filter = ('status', 'author')  # acsending or deseding sorting
     # ordering = ('created_data',)    # ordering by
     search_fields = ['title', 'content']    # looking for an expression in the title or the content
+    summernote_fields = '__all__'
     
 admin.site.register(Post, Post_Admin)
 admin.site.register(Category)
