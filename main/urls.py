3b898853781677da8ba0b638120e8eb2fe19f22d
theharez <rahimi.alireza2109@gmail.com>
2022-10-19 14:09:33 +0330

    Update urls.py
    add debug url
    add summernote url
    add captcha url

--- main/urls.py
+++ main/urls.py
@@ -32,7 +32,10 @@ urlpatterns = [
     path('', include('website.urls')),
     path('blog/', include('blog.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    path('robots.txt', include('robots.urls'))
+    path('robots.txt', include('robots.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
+    path('summernote/', include('django_summernote.urls')),
+    path('captcha/', include('captcha.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
