b675d834637b8769a4435c80d6f10f8f6c342417
theharez <rahimi.alireza2109@gmail.com>
2022-10-25 14:42:28 +0330

    Update forms.py

--- website/forms.py
+++ website/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from website.models import Contact, news_letter
 from captcha.fields import CaptchaField
 
+
 class ContactForm(forms.ModelForm):
     captcha = CaptchaField()
     class Meta:
         model = Contact
         fields = '__all__'
-
 
 class news_letter_form(forms.ModelForm):
     class Meta:
