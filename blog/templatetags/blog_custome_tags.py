aaf3a1a23f97489e1a48436296908596ff2e8c16
theharez <rahimi.alireza2109@gmail.com>
2022-10-27 23:52:20 +0330

    Update blog_custome_tags.py
    add home blog page comment counter

--- blog/templatetags/blog_custome_tags.py
+++ blog/templatetags/blog_custome_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Comment
 from blog.models import Category
 register = template.Library()
 
@@ -24,6 +24,10 @@ def func():
 def func():
     posts = Post.objects.filter(status=1)
     return posts
+
+@register.simple_tag()
+def comment(pid):
+    return Comment.objects.filter(post=pid, approved=True).count()
 
 
 @register.inclusion_tag('blog/blog-latest-posts.html', name='latest')
