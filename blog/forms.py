bd202291591b4450005c521aa593510d806d4719
theharez <rahimi.alireza2109@gmail.com>
2022-10-27 23:51:11 +0330

    Create forms.py
    comment section

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from django import forms
+from blog.models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['post', 'name', 'email', 'subject', 'message']
+        
