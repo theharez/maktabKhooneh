458ac374177587f478b12f9b8b87f08343611465
theharez <rahimi.alireza2109@gmail.com>
2022-10-31 18:14:10 +0330

    Update urls.py
    users managment

--- main/urls.py
+++ main/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
     path('blog/', include('blog.urls')),
+    path('users/', include('users.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt', include('robots.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
