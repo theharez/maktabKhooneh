654a0b124fd8af53fe322752ef1e4083d223611c
theharez <rahimi.alireza2109@gmail.com>
2022-11-03 19:59:17 +0330

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ app_name = 'users'
 urlpatterns = [
     path('login', login_view, name='login'),
     path('logout', logout_view, name='logout'),
-    path('login', singup_view, name='signup')
+    path('signup', signup_view, name='signup')
 ]
