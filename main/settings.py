6e85ef4adfe02cc9cbedadfce6181bd4310ec7fd
theharez <rahimi.alireza2109@gmail.com>
2022-12-18 20:33:46 +0330

    Update settings.py
    smtp protocol configurations

--- main/settings.py
+++ main/settings.py
@@ -137,3 +137,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL= '/users/login'
 
 CSRF_COOKIE_SECURE = True 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'alirr2109@gmail.com'
+EMAIL_HOST_PASSWORD = 'yfhjlvuhbbkvaqeo'
+EMAIL_PORT = 587
