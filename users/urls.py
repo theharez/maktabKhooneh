1630789dbafa9329555f14b41309fd5d4b8c5315
theharez <rahimi.alireza2109@gmail.com>
2022-12-18 22:23:02 +0330

    Update urls.py
    add forgot password url

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,7 @@ app_name = 'users'
 urlpatterns = [
     path('login', login_view, name='login'),
     path('logout', logout_view, name='logout'),
-    path('signup', signup_view, name='signup')
+    path('signup', signup_view, name='signup'),
+    path('email', email_view, name='email'),
+    path('password', password_view, name='password')
 ]
