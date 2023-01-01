70d15533a055b1d6d50ff86532201c88b89c60ac
theharez <rahimi.alireza2109@gmail.com>
2023-01-01 20:52:02 +0330

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,6 @@
 
-from django.forms import ModelForm
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 
